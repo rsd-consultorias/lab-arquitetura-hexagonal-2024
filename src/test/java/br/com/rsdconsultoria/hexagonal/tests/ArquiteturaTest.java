c3936698d82d70d403fccb83a1a4cae5f5a07192
Rafael Dias <rafael.dias@rsdconsultoria.com.br>
2024-09-10 06:59:31 -0300

    unit test

--- src/test/java/br/com/rsdconsultoria/hexagonal/tests/ArquiteturaTest.java
+++ src/test/java/br/com/rsdconsultoria/hexagonal/tests/ArquiteturaTest.java
@@ -4,50 +4,24 @@ import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
 import com.tngtech.archunit.core.importer.ImportOption;
 import com.tngtech.archunit.lang.ArchRule;
-import com.tngtech.archunit.library.Architectures;
 import org.junit.jupiter.api.Test;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 
 public class ArquiteturaTest {
 
-    // @Test
-    // void servicosDevemEstarNoPacoteService() {
-    // JavaClasses importedClasses = new
-    // ClassFileImporter().importPackages("br.com.rsdconsultoria.hexagonal");
-
-    // ArchRule regra = classes()
-    // .that().resideInAPackage("..application.service..")
-    // .should().onlyBeAccessed().byAnyPackage("..application.service..",
-    // "..application.port..");
-
-    // regra.check(importedClasses);
-    // }
-
-    // @Test
-    // void arquiteturaEmCamadas() {
-    //     JavaClasses importedClasses = new ClassFileImporter()
-    //             .withImportOption(ImportOption.Predefined.DO_NOT_INCLUDE_TESTS)
-    //             .importPackages("br.com.rsdconsultoria.hexagonal");
-
-    //     ArchRule regra = Architectures.onionArchitecture()
-    //             .applicationServices("br.com.rsdconsultoria.hexagonal.application.service..")
-    //             .domainServices("br.com.rsdconsultoria.hexagonal.domain.service..")
-    //             .domainModels("br.com.rsdconsultoria.hexagonal.domain.model..");
-
-    //     regra.check(importedClasses);
-    // }
-
     @Test
-    void adaptadoresNaoDevemAcessarDiretamenteRepositorios() {
+    void garantirArquitetura() {
         JavaClasses importedClasses = new ClassFileImporter()
                 .withImportOption(ImportOption.Predefined.DO_NOT_INCLUDE_TESTS)
-                .importPackages("br.com.rsdconsultoria.hexagonal");
+                .importPackages("br.com.rsdconsultoria.hexagonal", "java.lang..", "java.util..", "java.math..");
 
-        ArchRule regra = classes()
+        // Domain Services só podem ser acessadas pela Application Service
+        ArchRule domainServices = classes()
                 .that().resideInAPackage("br.com.rsdconsultoria.hexagonal.domain.service..")
                 .should().onlyBeAccessed().byAnyPackage("br.com.rsdconsultoria.hexagonal.application..");
 
-        regra.check(importedClasses);
+        domainServices.check(importedClasses);
+
     }
 }
